625ec6fe4690937f88d63128775806ef444fef88
Tony <tony.zhao.australia@gmail.com>
2017-07-09 15:37:58 +1000

    add auto deploy, again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! env-production');
+  res.send('Hello World! env-production auto deploy');
 });
 
 var server = app.listen(3000, function () {
